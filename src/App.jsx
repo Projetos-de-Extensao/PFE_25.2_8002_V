c9c34359a93a9458095177e9b4a5333d5b62e29c
Caio Ximenes <caio.ximenesmg@gmail.com>
2025-10-29 13:43:41 -0300

    feat:Router criado e organizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 import JobsFeedPage from './pages/Student/JobsFeedPage/JobsFeedPage.jsx';
+import Myjobs from './pages/Professor/MyJobs/Myjobs.jsx';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<JobsFeedPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <JobsFeedPage />,
+  },
+  {
+    path: "/myjobs",
+    element: <Myjobs />,
+  }
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
---
+++ src/pages/Professor/MyJobs/Myjobs.jsx
@@ -0,0 +1,12 @@
+import styles from './Myjobs.module.scss'
+
+
+const Myjobs = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Minhas Vagas</h1>
+        </div>
+    )
+}
+
+export default Myjobs
