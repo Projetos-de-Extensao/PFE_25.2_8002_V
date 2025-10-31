96481a15302cc359945410e1e9aaa1a1d3575bbc
Caio Ximenes <caio.ximenesmg@gmail.com>
2025-10-31 12:41:23 -0300

    Tela de Erro Servidor

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } fro
 import JobsFeedPage from './pages/Student/JobsFeedPage/JobsFeedPage.jsx';
 import Myjobs from './pages/Professor/MyJobs/Myjobs.jsx';
 import Login from './pages/Shared/Login/Login.jsx';
+import ErrorPage from "./pages/App/ErrorPage.jsx";
 
 let router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/myjobs",
---
+++ src/pages/App/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Styles from './ErrorPage.module.scss';
+import Triste from '../../assets/imgs/carinhatriste.png';
+import PageHeader from '../../components/Layout/PageHeader/PageHeader';
+import Return from '../../assets/imgs/return.png'
+
+const ErrorPage = () => {
+  return (
+    <div className={Styles["error-page"]}>
+        <PageHeader title="404" />
+        <div className={Styles['message']}>
+            <img src={Triste} className={Styles['message__img']} alt="" />
+            Oops! Parece que não temos o que procura...
+        </div>
+        <a href="/feed" className={Styles['message__link']}>Voltar <img src={Return} alt="seta de voltar" className={Styles['message__link-icon']}/></a>
+    </div>
+  )
+}
+
+export default ErrorPage;
